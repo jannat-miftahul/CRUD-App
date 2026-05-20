8d63dbe992c79240f0351f248231df480f47c224
jannat-miftahul <jannat27120@gmail.com>
2026-05-20 23:25:53 +0600

    Add navigation bar

--- header.php
+++ header.php
@@ -15,6 +15,35 @@
 </head>
 
 <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">CRUD App</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="add_user.php">Add User</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">View Users</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
 
 
 
